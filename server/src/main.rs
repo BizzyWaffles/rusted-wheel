fa0b3c6e7d9dd53021281783a239da1dc4d7219c
TheBizzle <jason.bertsche@gmail.com>
2017-05-27 16:31:03 -0500

    Feature: Serve up client from server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,17 @@
 extern crate iron;
+extern crate router;
+extern crate mount;
+extern crate staticfile;
+
+use std::collections::HashMap;
+use std::path::Path;
 
 use iron::prelude::*;
+use iron::{Handler};
 use iron::status;
+
+use mount::Mount;
+use staticfile::Static;
 
 struct Api(f32);
 
@@ -12,6 +22,7 @@ impl Api {
 }
 
 fn main() {
+
     Iron::new(|req: &mut Request| {
         let api = Api (0.1);
 
@@ -34,6 +45,16 @@ fn main() {
         // println!("{}", req.body);
         Ok(Response::with((status::Ok, "Hello, world!")))
     }).http("localhost:3000").unwrap();
+
+    let mut router = router::Router::new();
+
+    let mut assets_mount = Mount::new();
+    assets_mount
+        .mount("/", router)
+        .mount("/client", Static::new(Path::new("../client")));
+
+    Iron::new(assets_mount).http("localhost:3000").unwrap();
+
 }
 
 // NOTE: a "'static" lifetime means that this refence is used statically when borrowed
