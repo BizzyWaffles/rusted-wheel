7ce2708fe7f554404aad90fa39901425dfdc707f
fluffywaffles <skorlir@gmail.com>
2017-07-01 15:27:20 -0500

    Wip: Use public IP and create tandem WS server

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,7 @@ extern crate mount;
 extern crate staticfile;
 extern crate uuid;
 extern crate time;
+extern crate ws;
 
 use std::thread;
 use std::io::Read;
@@ -125,14 +126,51 @@ fn main() {
         .mount("/client", Static::new(Path::new("../client")));
 
     let webserver_thread = thread::spawn(move || {
-        Iron::new(assets_mount).http("localhost:3000").unwrap()
+        Iron::new(assets_mount).http("10.105.144.17:3000").unwrap()
     });
 
     let websocket_thread = thread::spawn(move || {
-        loop {
-            println!("hi from websocket_thread");
-            thread::sleep(time::Duration::seconds(5).to_std().unwrap())
+        struct Server {
+            out: ws::Sender,
         }
+
+        impl ws::Handler for Server {
+            fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
+                // TODO
+                println!("ws:rcv[{}]: user with uuid {} sent ws msg {}",
+                         time::precise_time_ns(),
+                         "[[[ we don't have uuids in ws yet ]]]",
+                         msg);
+                Ok(())
+            }
+
+            fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
+                // TODO
+                println!("ws:opn[{}]: user with uuid {} opened ws cxn",
+                         time::precise_time_ns(),
+                         "[[[ we don't have uuids in ws yet ]]]");
+                Ok(())
+            }
+
+            fn on_close(&mut self, code: ws::CloseCode, reason: &str) {
+                println!("ws:cls[{}]: user with uuid {} closed ws cxn\n\tCode [{:?}] reason: {}",
+                         time::precise_time_ns(),
+                         "[[[ we don't have uuids in ws yet ]]]",
+                         code,
+                         reason);
+                // TODO
+            }
+
+            fn on_error(&mut self, err: ws::Error) {
+                println!("ws:err[{}]: user with uuid {} got error {}",
+                         time::precise_time_ns(),
+                         "[[[ we don't have uuids in ws yet ]]]",
+                         err);
+                // TODO
+            }
+        }
+
+        ws::listen("10.105.144.17:3001", |out| Server { out: out }).unwrap()
     });
 
     // NOTE(jordan): Join all these threads so that we see stdout and stderr.
