afdfcee4c7266751d21d7d28f1597d90a5690ce0
fluffywaffles <skorlir@gmail.com>
2017-08-28 19:34:29 -0500

    Refactor/Minor: Authorizer returns authorized connection

--- server/src/sock/authorizer.rs
+++ server/src/sock/authorizer.rs
@@ -1,33 +1,34 @@
-use ws;
 use uuid::Uuid;
+use ws::util::Token;
+use super::Connection;
 use super::ConnectionMap;
 
 pub trait AuthorizesTicket<T> {
-    fn authorize_ticket(&self, token: ws::util::Token, ticket: T) -> Result<(), String>;
+    fn authorize_ticket(&self, token: Token, ticket: T) -> Result<Connection, String>;
 }
 
 pub struct DumbTicketStamper {
-    conn_map: ConnectionMap
+    connections: ConnectionMap
 }
 
 impl DumbTicketStamper {
     pub fn new(conn_map: ConnectionMap) -> DumbTicketStamper {
         DumbTicketStamper {
-            conn_map: conn_map
+            connections: conn_map
         }
     }
 }
 
 impl AuthorizesTicket<Uuid> for DumbTicketStamper {
-    fn authorize_ticket(&self, token: ws::util::Token, ticket: Uuid) -> Result<(), String> {
+    fn authorize_ticket(&self, token: Token, ticket: Uuid) -> Result<Connection, String> {
         println!("authorizing ticket {:?}", ticket);
-        let conn_map = self.conn_map.borrow();
-        conn_map.get(&token)
+        self.connections.borrow()
+            .get(&token)
             .ok_or(String::from("authorization failed: no connection for token"))
             .and_then(|conn| {
                 if conn.ticket == ticket {
                     println!("authorization success");
-                    Ok(())
+                    Ok(conn.clone())
                 } else {
                     Err(String::from("authorization failed: ticket mismatch"))
                 }
